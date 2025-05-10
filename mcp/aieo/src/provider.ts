00ff84f29398b609bb1090e609bc301d89ffb8d8
Evanfeenstra <evanfeenstra@gmail.com>
2025-05-09 20:21:20 -0700

    aieo stream provider

---
+++ mcp/aieo/src/provider.ts
@@ -0,0 +1,65 @@
+import { CoreMessage, streamText } from "ai";
+import { createAnthropic, AnthropicProviderOptions } from "@ai-sdk/anthropic";
+import {
+  createGoogleGenerativeAI,
+  GoogleGenerativeAIProviderOptions,
+} from "@ai-sdk/google";
+import { createOpenAI, OpenAIResponsesProviderOptions } from "@ai-sdk/openai";
+
+export type Provider = "anthropic" | "google" | "openai";
+
+export const PROVIDERS: Provider[] = ["anthropic", "google", "openai"];
+
+const SOTA = {
+  anthropic: "claude-3-7-sonnet-20250219",
+  google: "gemini-2.5-pro-preview-05-06",
+  openai: "gpt-4.1",
+};
+
+export async function getModel(provider: Provider, apiKey: string) {
+  switch (provider) {
+    case "anthropic":
+      const anthropic = createAnthropic({
+        apiKey,
+      });
+      return anthropic(SOTA[provider]);
+    case "google":
+      const google = createGoogleGenerativeAI({
+        apiKey,
+      });
+      return google(SOTA[provider]);
+    case "openai":
+      const openai = createOpenAI({
+        apiKey,
+        compatibility: "strict",
+      });
+      return openai(SOTA[provider]);
+    default:
+      throw new Error(`Unsupported provider: ${provider}`);
+  }
+}
+
+export function getProviderOptions(provider: Provider) {
+  switch (provider) {
+    case "anthropic":
+      return {
+        anthropic: {
+          thinking: { type: "enabled", budgetTokens: 24000 },
+        } satisfies AnthropicProviderOptions,
+      };
+    case "google":
+      return {
+        google: {
+          thinkingConfig: {
+            thinkingBudget: 16384,
+          },
+        } satisfies GoogleGenerativeAIProviderOptions,
+      };
+    case "openai":
+      return {
+        openai: {} satisfies OpenAIResponsesProviderOptions,
+      };
+    default:
+      throw new Error(`Unsupported provider: ${provider}`);
+  }
+}
---
+++ mcp/aieo/src/stream.ts
@@ -0,0 +1,33 @@
+import { CoreMessage, streamText } from "ai";
+import { Provider, getModel, getProviderOptions } from "./provider";
+
+export async function callModel(
+  provider: Provider,
+  apiKey: string,
+  messages: CoreMessage[],
+  parser?: (fullResponse: string) => void
+): Promise<string> {
+  const model = await getModel(provider, apiKey);
+  const providerOptions = getProviderOptions(provider);
+  console.log(`Calling ${provider} with options:`, providerOptions);
+  const result = streamText({
+    model,
+    messages,
+    temperature: 0,
+    providerOptions: providerOptions as any,
+  });
+  let fullResponse = "";
+  for await (const part of result.fullStream) {
+    switch (part.type) {
+      case "error":
+        throw part.error;
+      case "text-delta":
+        if (parser) {
+          parser(part.textDelta);
+        }
+        fullResponse += part.textDelta;
+        break;
+    }
+  }
+  return fullResponse;
+}
