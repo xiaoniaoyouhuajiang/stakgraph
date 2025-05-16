68217acb3b907053a43286d9439e97494241097f
Evanfeenstra <evanfeenstra@gmail.com>
2025-05-16 16:15:44 -0700

    aieo@0.1.8

--- mcp/aieo/src/index.ts
+++ mcp/aieo/src/index.ts
@@ -3,5 +3,5 @@ export * from "./provider";
 export * from "./stream";
 export * from "./prompt";
 
-export { type CoreMessage } from "ai";
-export { type Tool, type ToolSet } from "ai";
+export type { CoreMessage } from "ai";
+export type { Tool, ToolSet } from "ai";
