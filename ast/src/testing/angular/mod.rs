7d5757b7c5bf072be3db9228b8dc49b8a3411bf3
MuhammadUmer44 <muhammadumerakhtar51@gmail.com>
2025-06-11 16:39:21 +0500

    Fix Angular component test expectations

--- ast/src/testing/angular/mod.rs
+++ ast/src/testing/angular/mod.rs
@@ -17,8 +17,8 @@ pub async fn test_angular_generic<G: Graph>() -> Result<(), anyhow::Error> {
     let graph = repo.build_graph_inner::<G>().await?;
 
     let (num_nodes, num_edges) = graph.get_graph_size();
-    assert_eq!(num_nodes, 80, "Expected 80 nodes");
-    assert_eq!(num_edges, 90, "Expected 90 edges");
+    assert_eq!(num_nodes, 93, "Expected 93 nodes");
+    assert_eq!(num_edges, 95, "Expected 95 edges");
 
     let imports = graph.find_nodes_by_type(NodeType::Import);
     assert_eq!(imports.len(), 10, "Expected 10 imports");
