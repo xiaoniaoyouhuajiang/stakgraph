8e5525bd54dd60c6a5f5cdf3cc1ae9784131db2c
kelmith <faye.12222883@lpu.in>
2025-08-24 19:21:39 +0530

    tests updatee

--- ast/src/testing/nextjs/mod.rs
+++ ast/src/testing/nextjs/mod.rs
@@ -38,7 +38,7 @@ pub async fn test_nextjs_generic<G: Graph>() -> Result<()> {
 
     let file_nodes = graph.find_nodes_by_type(NodeType::File);
     nodes += file_nodes.len();
-    assert_eq!(file_nodes.len(), 24, "Expected 24 File nodes");
+    assert_eq!(file_nodes.len(), 33, "Expected 33 File nodes");
 
     let card_file = file_nodes
         .iter()
@@ -205,11 +205,11 @@ pub async fn test_nextjs_generic<G: Graph>() -> Result<()> {
 
     let calls = graph.count_edges_of_type(EdgeType::Calls);
     edges += calls;
-    assert_eq!(calls, 50, "Expected 50 Calls edges");
+    assert_eq!(calls, 54, "Expected 54 Calls edges");
 
     let contains = graph.count_edges_of_type(EdgeType::Contains);
     edges += contains;
-    assert_eq!(contains, 130, "Expected 130 Contains edges");
+    assert_eq!(contains, 162, "Expected 162 Contains edges");
 
     let handlers = graph.count_edges_of_type(EdgeType::Handler);
     edges += handlers;
@@ -217,11 +217,16 @@ pub async fn test_nextjs_generic<G: Graph>() -> Result<()> {
 
     let tests = graph.find_nodes_by_type(NodeType::UnitTest);
     nodes += tests.len();
-    assert_eq!(tests.len(), 10, "Expected 10 UnitTest nodes");
+    assert_eq!(tests.len(), 23, "Expected 23 UnitTest nodes");
 
+    let integration_test = graph.find_nodes_by_type(NodeType::IntegrationTest);
+    nodes += integration_test.len();
+    assert_eq!(integration_test.len(), 4, "Expected 4 IntegrationTest nodes");
+
+    
     let e2e_tests = graph.find_nodes_by_type(NodeType::E2eTest);
     nodes += e2e_tests.len();
-    assert_eq!(e2e_tests.len(), 1, "Expected 1 E2eTest nodes");
+    assert_eq!(e2e_tests.len(), 4, "Expected 4 E2eTest nodes");
 
     let import = graph.count_edges_of_type(EdgeType::Imports);
     edges += import;
@@ -233,7 +238,7 @@ pub async fn test_nextjs_generic<G: Graph>() -> Result<()> {
 
     let import_nodes = graph.find_nodes_by_type(NodeType::Import);
     nodes += import_nodes.len();
-    assert_eq!(import_nodes.len(), 14, "Expected 14 Import nodes");
+    assert_eq!(import_nodes.len(), 17, "Expected 17 Import nodes");
 
     let datamodels = graph.find_nodes_by_type(NodeType::DataModel);
     nodes += datamodels.len();
